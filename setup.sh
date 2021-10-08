eae1ca8904800f4b2f891d6a4077e3f2ee64ef5d
pokehah <43659819+pokehah@users.noreply.github.com>
2021-10-08 11:06:09 -0500

    Verbosity options for setup.sh enabled.

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 
 ConfigPath="$(pwd)"
 
-mkdir -p "${HOME}"/.config/nvim
+mkdir -vp "${HOME}"/.config/nvim
 ln -s "${ConfigPath}"/init.vim "${HOME}"/.config/nvim/init.vim
 
-mkdir -p "${HOME}"/.config/tmux
-ln -s "${ConfigPath}"/tmux.conf "${HOME}"/.config/tmux/tmux.conf
+mkdir -vp "${HOME}"/.config/tmux
+ln -vs "${ConfigPath}"/tmux.conf "${HOME}"/.config/tmux/tmux.conf
 
 if [[ ! -f "${HOME}/.bash_aliases" ]]; then
-    ln -s "${ConfigPath}"/.bash_aliases "${HOME}"/.bash_aliases
+    ln -vs "${ConfigPath}"/.bash_aliases "${HOME}"/.bash_aliases
 else
     echo "\"${HOME}/.bash_aliases\" already exists, cannot link."
     # echo "Would you like to prepend existing config with this one?"
